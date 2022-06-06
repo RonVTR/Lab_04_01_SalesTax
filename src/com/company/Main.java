53a1f76762aa23be1c83bb900d38fb77ce67b7d5
aaronvictor <aaron.victor30@gmail.com>
2022-06-05 22:07:08 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double itemPrice = 10.50;
+        System.out.println(itemPrice * .05 + itemPrice + " is your total");
+        System.out.println(itemPrice * .05 + " is the sales tax");
+
+
+
+    }
+}
